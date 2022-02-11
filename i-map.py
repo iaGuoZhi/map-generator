6860f7f0dc135ddc518921875cd9ac8f7f5f5d3c
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2022-02-11 21:09:42 +0800

    Pick positions to put box, which can on behalf of river

--- i-map.py
+++ i-map.py
@@ -1,60 +1,19 @@
 # - i-map
 
 import random
+import os
 
 # - Lists of rectangles
-shapes0 = {
-    1:{"x": 1, "y": 1},
-    2:{"x": 1, "y": 2},
-    3:{"x": 2, "y": 1}
-}
-
 shapes1 = {
-    1:{"x": 10, "y": 10},
-    2:{"x": 15, "y": 8},
-    3:{"x": 7, "y": 3},
-    4:{"x": 12, "y": 5},
-    5:{"x": 20, "y": 2}
-}
-
-shapes2 = {
-    1:{"x": 30, "y": 5},
-    2:{"x": 20, "y": 6},
-    3:{"x": 25, "y": 5}
-}
-
-shapes3 = {
-    1:{"x": 10, "y": 50},
-    2:{"x": 15, "y": 75},
-    3:{"x": 5, "y": 100}
-}
-
-shapes4 = {
-    1:{"x": 7, "y": 2},
-    2:{"x": 6, "y": 3},
-    3:{"x": 5, "y": 4}
-}
-
-shapes5 = {
-    1:{"x": 70, "y": 30}
-}
-
-presets = {
-    "1 - Pocket sized" : {"Set": 1, "a": 15, "b": 25, "shapes": shapes1, "c": 2, "l": 5, "p": "T"},
-    "2 - Long" : {"Set": 2, "a": 500, "b": 100, "shapes": shapes3, "c": 3, "l": 100, "p": "T"},
-    "3 - Mess" : {"Set": 3, "a": 36, "b": 100, "shapes": shapes0, "c": 0, "l": 1000, "p": "F"},
-    "4 - Box" : {"Set": 4, "a": 36, "b": 100, "shapes": shapes2, "c": 0, "l": 20, "p": "T"},
-    "5 - Islands" : {"Set": 5, "a": 25, "b": 75, "shapes": shapes4, "c": 0, "l": 25, "p": "T"},
-    "6 - Waterland" : {"Set": 6, "a": 20, "b": 50, "shapes": shapes0, "c": 1, "l": 1500, "p": "F"},
-    "7 - Blob" : {"Set": 7, "a": 36, "b": 100, "shapes": shapes5, "c": 20, "l": 3, "p": "F"},
+    1:{"x": 3, "y": 3},
 }
 
 # Function that creates the basic map, defines stuff like size, legend, positions on left/right side, ect
-def Start(s):
+def Start():
     global MAP
     global stuff
     global PIL
-    global Legend 
+    global Legend
     global shapes
     global presets
     global l
@@ -62,45 +21,19 @@ def Start(s):
     global b
     global A
     global c
-    global LS 
+    global LS
     global RS
     global p
     stuff = ["*", "@", "!", ".", "+", "%", "&", "$", "#"]
     PIL = []
     MAP = {}
-    if s == "1":
-        shapes = shapes1
-        l = 7
-        c = 2
-        a = 18
-        b = 40
-        p = "T"
-    elif s == "2":
-        shapes = shapes2
-        l = 15
-        c = 3
-        a = 36
-        b = 100
-        p = "T"
-    elif s == "3":
-        shapes = shapes2
-        l = 50
-        c = 4
-        a = 48
-        b = 191
-        p = "T"
-    else:
-        for i in presets:
-            print(i)
-        cmd = int(input(">"))
-        for i in presets:
-            if presets[i]["Set"] == cmd:
-                shapes = presets[i]["shapes"]
-                l = presets[i]["l"]
-                c = presets[i]["c"]
-                a = presets[i]["a"]
-                b = presets[i]["b"]
-                p = presets[i]["p"]
+    shapes = shapes1
+    l = 15
+    c = 3
+    size = os.get_terminal_size()
+    a = size.lines - 3
+    b = size.columns - 25
+    p = "T"
     A = a*b
     MAP = {}
     for x in range(A):
@@ -185,87 +118,46 @@ def PlaceB(i):
         y += 1
         x = 0
 
-# Function that randomly picks a location/rectangle(box)
-def AddB():
-    global Box
-    global A
-    global b
-    global a
-    Box = random.choice(list(shapes.keys()))
-    while True:
-            i = random.randint(-1,(A))
-            if CPlaceB(i) == True:
-                PlaceB(i)
-                return None
 
-# Function that smooths out long corners
-def Curve():
-    global MAP
+def PickLocations(begin, end):
+   global Points
+   begin_row = int(begin/b) 
+   begin_column = begin - begin_row * b
+   end_row = int(end/b)
+   end_column = end - end_row * b
+
+   mid_row = int((begin_row + end_row) / 2)
+   mid_column = int((begin_column + end_column) / 2)
+   print(mid_row, mid_column)
+   mid = mid_row * b + mid_column
+   if ((mid_row == begin_row) and (mid_column == begin_column)) or (mid in Points) :
+       return
+   else:
+       Points.append(mid)
+       PickLocations(begin, mid)
+       PickLocations(mid, end)
+
+# Function that design which locations to place box
+def DesignLocations():
+    global A
+    global a
     global b
-    global c
-    global RS
-    global LS
-    t = 0
-    while t <= c:
-        t += 1
-        for i in MAP:
-            if MAP[i] == "#":
-                Sides = 0
-                # - U
-                x = i - b
-                try:
-                    a = MAP[x]
-                except:
-                    a = "~"
-                if a == "~":
-                    Sides += 1
-                # - U
-                # - D
-                x = i + b
-                try:
-                    a = MAP[x]
-                except:
-                    a = "~"
-                if a == "~":
-                    Sides += 1
-                # - D
-                # - L
-                if i in LS:
-                    Sides += 1
-                else:
-                    x = i - 1
-                    try:
-                        a = MAP[x]
-                    except:
-                        a = "~"
-                    if a == "~":
-                        Sides += 1
-                # - L
-                # - R
-                if i in RS:
-                    Sides += 1
-                else:
-                    x = i + 1
-                    try:
-                        a = MAP[x]
-                    except:
-                        a = "~"
-                    if a == "~":
-                        Sides += 1
-                # - R
-                if Sides == 4:
-                    MAP[i] = "~"
-                elif Sides == 1 and t <= c:
-                    if random.randint(0, 50) == 1:
-                        MAP[i] = "~"
-                elif Sides == 2 and t <= c:
-                    if random.randint(0, 3) != 1:
-                        MAP[i] = "~"
-                elif Sides == 3 and t <= c:
-                    if random.randint(0, 5) != 1:
-                        MAP[i] = "~"
-                else:
-                    pass
+    global Points
+    Points = []
+    Points.append(0)
+    Points.append(A - 1)
+    PickLocations(0, A - 1)
+    print(len(Points))
+    for i in Points:
+        AddB(i)
+
+# Function that randomly picks a rectangle(box) and place on location i
+def AddB(i):
+    global Box
+    Box = random.choice(list(shapes.keys()))
+    #if CPlaceB(i) == True:
+    PlaceB(i)
+    return None
 
 # Function that replaces the outline of the rectangles with ascii art
 def Outline():
@@ -281,7 +173,7 @@ def Outline():
             try:
                 a = MAP[x]
             except:
-                a = "~"
+                a = " "
             if a == "~":
                 Sides["U"] = 1
             # - U
@@ -290,31 +182,31 @@ def Outline():
             try:
                 a = MAP[x]
             except:
-                a = "~"
+                a = " "
             if a == "~":
                 Sides["D"] = 1
             # - D
             # - L
             if i in LS:
-                Sides["L"] = 1
+                Sides["L"] = 0
             else:
                 x = i - 1
                 try:
                     a = MAP[x]
                 except:
-                    a = MAP[i]
+                    a = " "
                 if a == "~":
                     Sides["L"] = 1
             # - L
             # - R
             if i in RS:
-                Sides["R"] = 1
+                Sides["R"] = 0
             else:
                 x = i + 1
                 try:
                     a = MAP[x]
                 except:
-                    a = MAP[i]
+                    a = " "
                 if a == "~":
                     Sides["R"] = 1
             # - R
@@ -392,23 +284,16 @@ def LegendC():
         Legend[n] = " | " + i + " = " + Meaning[i]
         n += 1
     Legend[a - 1] = " +----------------------+"
-    MAP[b + 2] = "N"
-    MAP[b*2 + 1] = "W"
-    MAP[b*2 + 2] = "+"
-    MAP[b*2 + 3] = "E"
-    MAP[b*3 + 2] = "S"
-        
 
 # Main loop
 while True:
-    print("Small(1), Medium(2), or Large(3)")
-    print("More(4)")
+    print("Regenerate(1)")
     cmd = input(">")
-    Start(cmd)
-    for i in range(l):
-        AddB()
+    while cmd != "1":
+        cmd = input(">")
+    Start()
+    DesignLocations()
     print("")
-    Curve()
     Outline()
     Clear()
     AddStuff()
